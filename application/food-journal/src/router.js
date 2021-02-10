f046764474f50e0559c1f8cdb30066368c855193
Carla Leal <lealramoscarla@gmail.com>
2021-02-09 19:39:46 -0500

    Added vue-cli to dockerfile and added vue store/router

---
+++ application/food-journal/src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+import Login from './components/Login.vue';
+const routes = [
+	  {
+		    path: "/login",
+		    component: Login
+	  }
+]
+
+const router = new VueRouter({
+	  mode: "history",
+	  routes,
+});
+
+export default router;
---
+++ application/food-journal/src/store.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+	  state: {
+		    timer: 0,
+		    maxTime: '20:00:00',
+		    userId: null,
+		    userBestTime: null,
+		    favouritedArtists: []
+	  },
+	  mutations: {
+		    setCurrentTimerValue(state, newTime) {
+			      state.timer = newTime
+		    },
+		    setUserIdForSession(state, userId) {
+			      state.userId = userId
+		    },
+		    setCurrentUserBestTimeValue(state, bestTime) {
+			      state.userBestTime = bestTime
+		    },
+		    setFavouritedArtists(state, favouritedArtists) {
+			      state.favouritedArtists = favouritedArtists
+		    },
+		    setMaxTime(state, maxTime) {
+			      state.maxTime = maxTime
+		    },
+	  }
+})
